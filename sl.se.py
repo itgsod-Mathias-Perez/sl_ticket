6453f17d6c8fc6bbb51da689331f5b6ae66853d3
MathiasPerez <mathas.perezbustos@elev.it-gymnasiet.se>
2017-10-18 13:59:54 +0200

    SL ticket project

--- sl.se.py
+++ sl.se.py
@@ -1,11 +1,29 @@
-def fares(age, student=False, senior=False):
-    print age,student,senior
-    return 'hel'
+def fares(age, adult=False, student=False,gubbe=False):
+    if age>=20 and age <65:
+        if adult==True:
+            return "20kr"
+
+
+        elif student==True or gubbe==True:
+             return "15kr"
+
+        elif age>=20 and age <=65:
+         return 'Adult pays 20 corwns'
+    else:
+        return 'Student or old people pays 15 crowns'
+
+
+print fares(78)
+print fares(20)
+print fares(65)
+print fares(18)
 
 
 
 
 
-fares(10)
-fares(65,senior=True)
-fares(25,student=True)
+
+
+
+
+
